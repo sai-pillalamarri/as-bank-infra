371ab2f6779895038df3c6d40e4142b163b28879
Sai Pillalamarri <nagarajusai91@gmail.com>
2026-09-03 17:25:28 +0530

    feat(terraform): add CloudFront dev edge

---
+++ terraform/bootstrap/cloudfront.tf
@@ -0,0 +1,82 @@
+data "aws_cloudfront_cache_policy" "caching_disabled" {
+  name = "Managed-CachingDisabled"
+}
+
+data "aws_cloudfront_origin_request_policy" "all_viewer" {
+  name = "Managed-AllViewer"
+}
+
+data "aws_cloudfront_response_headers_policy" "security_headers" {
+  name = "Managed-SecurityHeadersPolicy"
+}
+
+resource "aws_cloudfront_distribution" "dev" {
+  origin {
+    domain_name = "origin-dev.aslearnings.online"
+    origin_id   = "as-bank-dev-alb"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+
+      origin_ssl_protocols = [
+        "TLSv1.2",
+      ]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "AS Bank dev edge"
+
+  aliases = [
+    "dev.aslearnings.online",
+    "api-dev.aslearnings.online",
+  ]
+
+  default_cache_behavior {
+    target_origin_id = "as-bank-dev-alb"
+
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT",
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
+
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer.id
+
+    response_headers_policy_id = data.aws_cloudfront_response_headers_policy.security_headers.id
+
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cloudfront.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  # The apply role gains CloudFront write access in the same Terraform run.
+  depends_on = [
+    aws_iam_role_policy.infrastructure_apply_bootstrap_write,
+  ]
+}
